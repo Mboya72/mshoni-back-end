5d850eddaf57c6769554a7907a14255ab363f149
Mboya72 <elvindio72@gmail.com>
2026-03-04 16:30:22 +0300

    feat: Define User model with role choices, username, email, and timestamps

--- users/models.py
+++ users/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('ADMIN', 'Admin'),
+        ('TAILOR', 'Tailor'),
+        ('CUSTOMER', 'Customer'),
+        ('SUPPORT', 'Support'),
+    )
+    
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        null=False,
+        blank=False,
+    )
+    email = models.EmailField(
+        unique=True,
+        null=False,
+        blank=False,
+    )
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='CUSTOMER',help_text="Role of the user in the system")
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.role})"
